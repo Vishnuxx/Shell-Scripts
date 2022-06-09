36bbf166b4b13178d1259d6d28c25d03726f6760
vishnuxx <vishnuvs2002it@gmail.com>
2022-06-10 00:56:52 +0530

    create_dir.sh

---
+++ create_dir.sh
@@ -0,0 +1,8 @@
+#create directiry temp and go inside it 
+mkdir temp && cd temp
+
+#create directiry testoo and go inside it 
+mkdir ./testoo && cd testoo
+
+#create three files inside testoo
+touch file01.txt   file02.txt  file03.txt && ls
